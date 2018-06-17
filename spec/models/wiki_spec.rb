f7eb8667b408a95be905ab9a70d8ed398a73c871
Douglas Schreck <dcschreck421@gmail.com>
2018-06-17 12:12:51 +0300

    add CRUD for wikis

---
+++ spec/models/wiki_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Wiki, type: :model do
+    let(:user) { User.create!(email: "test@aol.com", password: "password") }
+    let(:wiki) { Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_presence_of(:user) }
+
+    it { is_expected.to validate_length_of(:title).is_at_least(5) }
+    it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
+    describe "attributes" do
+        it "has title, body, and user attributes" do
+            expect(wiki).to have_attributes(title: wiki.title, body: wiki.body)
+        end
+    end
+end
